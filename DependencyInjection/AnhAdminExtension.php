902ccd7e099fe887733a19a29a1e21f25191655e
Andrew Hilobok <hilobok@gmail.com>
2013-10-22 16:00:35 +0300

    Form and basic styling

--- DependencyInjection/AnhAdminExtension.php
+++ DependencyInjection/AnhAdminExtension.php
@@ -6,13 +6,14 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class AnhAdminExtension extends Extension
+class AnhAdminExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -24,5 +25,22 @@ class AnhAdminExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $container->prependExtensionConfig('twig', array(
+            'form' => array(
+                'resources' => array(
+                    'AnhAdminBundle::fields.html.twig'
+                )
+            )
+        ));
+
+        $container->prependExtensionConfig('assetic', array(
+            'bundles' => array(
+                'AnhAdminBundle'
+            )
+        ));
     }
 }
